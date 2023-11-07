009229d2f813b4295b2ccb3966099dc891ef3658
amourayuba <amourayuba@gmail.com>
2023-11-07 17:21:04 -0500

    Added __init__.py files and notebooks

---
+++ Particles_in_halos/__init__.py
@@ -0,0 +1,4 @@
+from . import get_particles_in_grids
+from . import get_particles_in_halos
+__all__ = ['get_particles_in_grids', 'get_particles_in_halos']
+
---
+++ structural_properties/__init__.py
@@ -0,0 +1,3 @@
+from . import fits2ds
+
+__all__ = ['fits2ds']
